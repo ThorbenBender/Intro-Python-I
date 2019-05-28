a06381b9c9fdc2509819dcb57033469b19872431
Thorben Bender <bender.thorben0@gmail.com>
2019-05-28 14:04:04 +0200

    done with 05

--- src/04_printing.py
+++ src/04_printing.py
@@ -12,6 +12,8 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print("a=%d, y=%d, z=%d" % (f(x,n),g(x,n)));
+
 # Use the 'format' string method to print the same thing
 
 # Finally, print the same thing using an f-string
